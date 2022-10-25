c2635775d130785e725b578cd7a9d3ad66fb9f0c
Flávio Candido dos Santos Policarpo Silva <flaviocandidopolicarpo@hotmail.com>
2022-10-25 13:56:34 -0300

    autenticação de login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use App\Post;
+use App\Http\Requests\PostRequest;
 
 class LoginController extends Controller
 {
@@ -15,12 +17,10 @@ class LoginController extends Controller
         return view('register.register');
     }
 
-
     public function showIndex()
     {
         return view('login.login');
     }
-
 
     public function store(Request $request)
     {
@@ -33,10 +33,20 @@ class LoginController extends Controller
 
         $password = bcrypt($request->password);
 
+            $user = User::firstOrCreate(['email' => $request->email], [
+                'email' => $request->email,
+                'name' => $request->name,
+                'password' => $password
+            ]);
 
-        if (!User::create(['name' => $request->name, 'email' => $request->email, 'password' => $password])) {
-            return redirect()->route('register.register')->with('error', 'Usuário já cadastrado!');
-        }
+            if ($user->wasRecentlyCreated) {
+                toastr()->success('Usuário cadastrado com sucesso!');
+                return redirect()->route('login.login');
+            } else {
+                toastr()->error('Erro, usuário já cadastrado!');
+                return redirect()->route('register.register');
+            }
+
 
         return view('login.login');
     }
